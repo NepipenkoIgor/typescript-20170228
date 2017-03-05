7f94d8e4e5cb69b7239e03042c9d85242de2473f
Vadim <pv@petrovvadim.com>
2017-03-05 09:41:31 +0300

    Petrov Vadim folder added

---
+++ lesson-1/PetrovVadim/main.ts
@@ -0,0 +1,73 @@
+type menuList = {
+	title: string,
+	items?: menuList
+}[];
+
+const menuList: menuList = [{
+	title: 'Животные',
+	items: [{
+		title: 'Млекопитающие',
+		items: [
+			{ title: 'Коровы' },
+			{ title: 'Ослы' },
+			{ title: 'Собаки' },
+			{ title: 'Тигры' }
+		]
+	}, {
+		title: 'Другие',
+		items: [
+			{ title: 'Змеи' },{ 
+				title: 'Птицы',
+				items: [
+				{ title: 'Голуби',
+					items:[{
+						title: 'Крупные'
+					},{
+						title: 'Мелкие'
+					}] },
+				{ title: 'Вороны' }
+			] },
+			{ title: 'Ящерицы' },
+		],
+	}, ]
+}, {
+	title: 'Рыбы',
+	items: [{
+		title: 'Аквариумные',
+		items: [
+			{ title: 'Гуппи' },
+			{ title: 'Скалярии' }
+		]
+	}, {
+		title: 'Форель',
+		items: [
+			{ title: 'Морская форель' }
+		]
+	}, ]
+}];
+
+function generateMenu(list: menuList): string {
+
+	let content: string = `<ul>`;
+	for (let a of list) {
+		content += `<li><a class='title'>${a.title}</a>`;
+		if( a.items ) {
+			content += generateMenu(a.items);
+			content += `</li>`
+		}
+	}
+	content += `</ul>`
+	return content;
+}
+
+let navMenuList = document.querySelector('.menu') as HTMLDivElement;
+navMenuList.innerHTML = generateMenu(menuList);
+navMenuList.onclick = (ev: MouseEvent) => {
+	let el = ev.target as HTMLAnchorElement;
+	let classList = el.classList;
+	if (!classList.contains('title')) {
+		return;
+	}
+	let parentLi = el.parentNode as HTMLLIElement;
+	parentLi.classList.toggle('menu-open')
+}
