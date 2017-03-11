aa003bbd8a0bce4d8aebe6aaabb77e0c9351506b
moiseyev <alexander@moiseyev.name>
2017-03-11 23:42:31 +0100

    Решение второй задачи

---
+++ lesson-2/MoiseyevAlexander/task-2.js
@@ -0,0 +1,15 @@
+// Написать функцию summator(), которая сумирует переданые ей аргументы.
+// Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+function summator(...restOfArgs) {
+    let result = restOfArgs[0];
+    for (let i = 1; i < restOfArgs.length; i++) {
+        result += restOfArgs[i];
+    }
+    return result;
+}
+console.log(summator(1));
+console.log(summator(1, 2));
+console.log(summator(1, 2, 3));
+console.log(summator('один'));
+console.log(summator('один', 'два'));
+console.log(summator('один', 'два', 'три'));
